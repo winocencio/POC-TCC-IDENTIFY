376f96446ba0ab7edfbafb636c196f682e33f1a8
Willian Inocencio <willian.i.alves@hotmail.com>
2020-02-25 18:47:43 -0300

    Initial Commit

---
+++ REAME.MD
@@ -0,0 +1,11 @@
+## Reconhecer ovos de mosquito da dengue
+
+Instalar dependencia:  
+`pip install numpy`
+
+Executar Open Cv na [imagem.png](./img/imagem.png), criando uma outra imagem com a quantidade e com quadrado verde:  
+`python openCvCascade`
+
+Pode ser adicionado:
+- [ ] Transformar em uma função com parametros como "versao cascade, caminho da imagem, saida e entrada e etc"
+- [ ] Transformar openCvCascade em um objeto para ser importado em outro projeto, ex: Web
---
+++ openCvCascade.py
@@ -0,0 +1,19 @@
+import cv2
+
+image = cv2.imread('img/imagem.png')
+cascadeVersion = '0.1'
+
+classifier = cv2.CascadeClassifier('haarcascadeXml/cascadeV' + cascadeVersion + '.xml')
+imageGray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+deteccoes = classifier.detectMultiScale(imageGray)
+
+for (x,y,l,a) in deteccoes:
+    cv2.rectangle(image, (x,y), (x + l , y + a), (0,255,0),2)
+
+qtdDetectados = str(len(deteccoes)) + 'Detectados'
+print(qtdDetectados)
+
+cv2.imwrite( "img/"+qtdDetectados+".jpg", image )
+cv2.waitKey(0)
+cv2.destroyAllWindows()
