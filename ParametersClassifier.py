d93d3c583c9e5c808a85391bdd6e290f06fa38fe
Willian Inocencio <willian.i.alves@hotmail.com>
2020-08-04 22:24:43 -0300

    novos parametros no identificador

--- ParametersClassifier.py
+++ ParametersClassifier.py
@@ -14,11 +14,11 @@ class ParametersClassifier:
 # print(ParametersClassifier(1.1,1,(30,30),(40,40),0).getDict())
 
 def getParametrosVariados():
-    scaleFactorList = ['0.8','0.9','1.0','1.1','1.2','1.3','1.4','1.5']
-    minNeighborsList = ['0','1','2','3','4','5']
-    minSizeList = ['(0,0)','(10,10)','(15,15)','(20,20)','(25,25)','(30,30)']
-    maxSizeList = ['(30,30)','(35,35)','(40,40)','(45,45)','(50,50)','(60,60)']
-    flagsList = ['0']
+    scaleFactorList = [1.1,1.2,1.3,1.4,1.5,1.6]
+    minNeighborsList = [0,1,2,3,4,5]
+    minSizeList = [(0,0),(10,10),(15,15),(20,20),(25,25),(30,30)]
+    maxSizeList = [(30,30),(35,35),(40,40),(45,45),(50,50),(60,60)]
+    flagsList = [0]
 
     returnList = []
     for scaleFactor in scaleFactorList:
--- openCvCascade.py
+++ openCvCascade.py
@@ -1,30 +1,31 @@
 import cv2
 import glob
-from ParametersClassifier import ParametersClassifier
+from ParametersClassifier import ParametersClassifier , getParametrosVariados
 from Log import Log
 
 
-cascadeVersion = '0.3.2'
-images_path = [img for img in glob.glob("img/original/I-*")]
+cascadeVersion = '0.4.014'
+images_path = [img for img in glob.glob("img/original/I-1*")]
 images_name = [img[13:-4] for img in images_path]
 
-parameters_classifier = ParametersClassifier(1.1,1,(30,30),(40,40),0)
-index = 0
+# parameters_classifier = ParametersClassifier(1.1,1,(30,30),(40,40),0)
 classifier = cv2.CascadeClassifier('haarcascadeXml/cascadeV' + cascadeVersion + '.xml')
 
-for image_i in images_path:
+for parameters_classifier in getParametrosVariados():
+    index = 0  
+    for image_i in images_path:
 
-    image = cv2.imread(image_i)
-    imageGray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        image = cv2.imread(image_i)
+        imageGray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    deteccoes = classifier.detectMultiScale(imageGray, **parameters_classifier.getDict())
+        deteccoes = classifier.detectMultiScale(imageGray, **parameters_classifier.getDict())
 
-    for (x,y,l,a) in deteccoes:
-        center = (x + l//2, y + a//2)
-        cv2.ellipse(image, center, (l//2, a//2), 0, 0, 360, (255, 0, 255), 2)
+        for (x,y,l,a) in deteccoes:
+            center = (x + l//2, y + a//2)
+            cv2.ellipse(image, center, (l//2, a//2), 0, 0, 360, (255, 0, 255), 2)
 
-    log = Log(images_name[index],cascadeVersion,parameters_classifier,len(deteccoes),deteccoes)
-    log.logar()
+        log = Log(images_name[index],cascadeVersion,parameters_classifier,len(deteccoes),deteccoes)
+        log.logar()
 
-    cv2.imwrite( "img/resultados/" + images_name[index] + "-F/" + images_name[index] +"-Log-" + str(log.id) +  "-.jpg", image)
-    index+= 1
+        cv2.imwrite( "img/resultados/" + images_name[index] + "-F/" + images_name[index] +"-Log-" + str(log.id) +  "-.jpg", image)
+        index+= 1
